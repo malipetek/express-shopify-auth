3ce2b54b4239d74a123611b04342d9b844ac2f1b
muhammet ali petek <malipetek@gmail.com>
2022-03-14 03:21:45 +0000

    minor fix - accidentally left ctx

--- src/auth/create-top-level-redirect.ts
+++ src/auth/create-top-level-redirect.ts
@@ -5,7 +5,6 @@ import {Request, Response} from 'express';
 import redirectionPage from './redirection-page';
 
 export default function createTopLevelRedirect(apiKey: string, path: string) {
-
   return function topLevelRedirect(req: Request, res: Response) {
     const {query} = req;
     const {shop, host} = query;
@@ -14,11 +13,13 @@ export default function createTopLevelRedirect(apiKey: string, path: string) {
     const queryString = querystring.stringify(params);
 
     res.set('Content-Type', 'text/html');
-    res.send(redirectionPage({
-      origin: shop,
-      redirectTo: `https://${ctx.host}${path}?${queryString}`,
-      apiKey,
-      host,
-    }));
+    res.send(
+      redirectionPage({
+        origin: shop,
+        redirectTo: `https://${host}${path}?${queryString}`,
+        apiKey,
+        host,
+      }),
+    );
   };
 }
